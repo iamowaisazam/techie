c0f33f54860bc4da1c8d40cde9bd7841173e99c7
iamowaisazam <iamowaisazam@gmail.com>
2020-10-18 12:49:36 +0500

    Updates

---
+++ precache-manifest.812ef1fa7e384b0ca700cccbc8b74aa6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2a010641d63c111fe83d636139e91fe",
+    "url": "/techie/index.html"
+  },
+  {
+    "revision": "1d064667f56f58b1c21a",
+    "url": "/techie/static/css/2.a1063ac4.chunk.css"
+  },
+  {
+    "revision": "8914bc35e59a0ee3843e",
+    "url": "/techie/static/css/main.cf76b579.chunk.css"
+  },
+  {
+    "revision": "1d064667f56f58b1c21a",
+    "url": "/techie/static/js/2.75cbb11f.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/techie/static/js/2.75cbb11f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8914bc35e59a0ee3843e",
+    "url": "/techie/static/js/main.95517bfc.chunk.js"
+  },
+  {
+    "revision": "9896b3b61b9d3ef01417",
+    "url": "/techie/static/js/runtime-main.8535a474.js"
+  },
+  {
+    "revision": "28529a74e69293e18afd40fb2153239c",
+    "url": "/techie/static/media/cover.28529a74.jpg"
+  }
+]);
--- src/config/Modal.js
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState,createContext } from 'react';
-
-const Modal = (props) => {
-   
-return <> 
-
-</> 
-}
-export default Modal;
