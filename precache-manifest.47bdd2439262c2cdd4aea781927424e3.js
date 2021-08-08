4e49f7032050bf5ddf85c1d4fbc792e54ff44355
iamowaisazam <iamowaisazam@gmail.com>
2021-08-08 17:54:52 +0500

    Updates

---
+++ precache-manifest.47bdd2439262c2cdd4aea781927424e3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3755cc69e40dfcc177d74c678f85c93b",
+    "url": "/techie/index.html"
+  },
+  {
+    "revision": "50e910c6f767cbc3705c",
+    "url": "/techie/static/css/2.a1063ac4.chunk.css"
+  },
+  {
+    "revision": "5a1133342dc625df3395",
+    "url": "/techie/static/css/main.bba005ad.chunk.css"
+  },
+  {
+    "revision": "50e910c6f767cbc3705c",
+    "url": "/techie/static/js/2.c40eee44.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/techie/static/js/2.c40eee44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a1133342dc625df3395",
+    "url": "/techie/static/js/main.a33a1307.chunk.js"
+  },
+  {
+    "revision": "9896b3b61b9d3ef01417",
+    "url": "/techie/static/js/runtime-main.8535a474.js"
+  },
+  {
+    "revision": "8fd6b28bff7821536ba09a666f60ae6f",
+    "url": "/techie/static/media/about.8fd6b28b.jpg"
+  },
+  {
+    "revision": "28529a74e69293e18afd40fb2153239c",
+    "url": "/techie/static/media/cover.28529a74.jpg"
+  },
+  {
+    "revision": "54af723cad3b48fa351c0037246ce0a0",
+    "url": "/techie/static/media/features.54af723c.png"
+  },
+  {
+    "revision": "2f461eccf9292ef77f2a01a5147206c1",
+    "url": "/techie/static/media/hero-img.2f461ecc.png"
+  }
+]);
